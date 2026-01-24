2b4b1711799d72b39cd1c01f58135e9ca8b7398b
cameron turner <17177161+cturner8@users.noreply.github.com>
2026-01-24 19:28:09 +0000

    init cobra app

---
+++ cmd/root.go
@@ -0,0 +1,31 @@
+/*
+Copyright © 2026 cturner8
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "binmate",
+	Short: "An application for managing binary installations from remote repositories.",
+	Long: `An application for managing binary installations from remote repositories.`,
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+
