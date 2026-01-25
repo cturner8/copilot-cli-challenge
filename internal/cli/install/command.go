a340ccfae85bda5e87b18d2176e42f8859bce76c
cameron turner <17177161+cturner8@users.noreply.github.com>
2026-01-25 18:59:29 +0000

    update install command to accept config argument, update argument checking

--- internal/cli/install/command.go
+++ internal/cli/install/command.go
@@ -6,23 +6,24 @@ import (
 
 	"github.com/spf13/cobra"
 
+	"cturner8/binmate/internal/core/config"
 	"cturner8/binmate/internal/core/install"
 	"cturner8/binmate/internal/providers/github"
 )
 
-func NewCommand() *cobra.Command {
+func NewCommand(config config.Config) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:     "install",
 		Short:   "Install a new binary version",
 		Aliases: []string{"i", "add"},
 		Run: func(cmd *cobra.Command, args []string) {
 			binary, err := cmd.Flags().GetString("binary")
-			if err != nil {
+			if err != nil || binary == "" {
 				log.Panicf("binary is required")
 			}
 
 			version, err := cmd.Flags().GetString("version")
-			if err != nil {
+			if err != nil || version == "" {
 				log.Panicf("version is required")
 			}
 
