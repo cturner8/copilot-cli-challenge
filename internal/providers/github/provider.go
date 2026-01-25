8163b082ccd29f6626683a4e792d8243e463e055
cameron turner <17177161+cturner8@users.noreply.github.com>
2026-01-25 17:53:41 +0000

    refactor asset download process

--- internal/providers/github/provider.go
+++ internal/providers/github/provider.go
@@ -5,21 +5,43 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
-func DownloadAsset(path string, version string, assetName string) string {
+func DownloadAsset(path string, version string, assetName string) (string, error) {
 	url := fmt.Sprintf("https://github.com/%s/releases/download/%s/%s", path, version, assetName)
 
 	resp, err := http.Get(url)
 	if err != nil {
-		panic("error downloading asset")
+		return "", fmt.Errorf("download asset: %w", err)
 	}
 	defer resp.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("download asset: unexpected status %s", resp.Status)
+	}
 
-	fileName := fmt.Sprintf("/tmp/%s", assetName)
-	os.WriteFile(fileName, body, 0755)
+	destPath := filepath.Join(os.TempDir(), assetName)
+	tmp, err := os.CreateTemp(os.TempDir(), assetName+".*")
+	if err != nil {
+		return "", fmt.Errorf("create temp file: %w", err)
+	}
 
-	return fileName
+	if _, err := io.Copy(tmp, resp.Body); err != nil {
+		tmp.Close()
+		os.Remove(tmp.Name())
+		return "", fmt.Errorf("write asset: %w", err)
+	}
+
+	if err := tmp.Close(); err != nil {
+		os.Remove(tmp.Name())
+		return "", fmt.Errorf("close temp file: %w", err)
+	}
+
+	if err := os.Rename(tmp.Name(), destPath); err != nil {
+		os.Remove(tmp.Name())
+		return "", fmt.Errorf("finalise asset: %w", err)
+	}
+
+	return destPath, nil
 }
