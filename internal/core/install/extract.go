d42146ba432f350c059cf94125df0b00aecd1239
cameron turner <17177161+cturner8@users.noreply.github.com>
2026-01-26 21:55:40 +0000

    update inputs to extract asset function, update destination path to user cache dir

--- internal/core/install/extract.go
+++ internal/core/install/extract.go
@@ -1,6 +1,18 @@
 package install
 
-func ExtractAsset(srcPath string, destDir string) error {
+import (
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func ExtractAsset(srcPath string, id string, version string) (string, error) {
+	cacheDir, err := os.UserCacheDir()
+	if err != nil {
+		log.Panicf("unable to locate asset cache dir")
+	}
+
+	destDir := filepath.Join(cacheDir, "binmate", id, version)
 	// TODO: check file type
 	return extractZip(srcPath, destDir)
 }
