ba30caf7d7300070d218141ff70d10f289b3c5cd
cameron turner <17177161+cturner8@users.noreply.github.com>
2026-01-27 19:32:01 +0000

    update extract asset to accept binary config as input

--- internal/core/install/extract.go
+++ internal/core/install/extract.go
@@ -1,29 +1,30 @@
 package install
 
 import (
+	"cturner8/binmate/internal/core/config"
 	"fmt"
 	"os"
 	"path/filepath"
 )
 
-func ExtractAsset(srcPath string, id string, version string, format string, binaryName string) (string, error) {
+func ExtractAsset(srcPath string, binary config.Binary, version string) (string, error) {
 	cacheDir, err := os.UserCacheDir()
 	if err != nil {
 		return "", fmt.Errorf("unable to locate asset cache dir")
 	}
 
-	destDir := filepath.Join(cacheDir, ".binmate", id, version)
+	destDir := filepath.Join(cacheDir, ".binmate", binary.Id, version)
 
-	switch format {
+	switch binary.Format {
 	case ".zip":
 		{
-			return extractZip(srcPath, destDir, binaryName)
+			return extractZip(srcPath, destDir, binary.Name)
 		}
 	case ".tar.gz":
 		{
-			return extractTar(srcPath, destDir, binaryName)
+			return extractTar(srcPath, destDir, binary.Name)
 		}
 	}
 
-	return "", fmt.Errorf("unsupported asset format: %s", format)
+	return "", fmt.Errorf("unsupported asset format: %s", binary.Format)
 }
