04e3635b6c73cab3db19c0328844bfc9f70abd87
cameron turner <17177161+cturner8@users.noreply.github.com>
2026-01-31 11:52:10 +0000

    rename get install path function, slight tidy up

--- internal/core/version/get_install_bin_path.go
+++ internal/core/version/get_install_bin_path.go
@@ -7,22 +7,22 @@ import (
 	"runtime"
 )
 
-func getInstallPath(userInstallPath string) (string, error) {
+func getInstallBinPath(userInstallPath string) (string, error) {
 	installPath := userInstallPath
 	if installPath == "" {
-		homeDir, err := os.UserHomeDir()
-		if err != nil {
-			return "", fmt.Errorf("unable to find user home directory: %s", err)
-		}
-
 		if runtime.GOOS == "windows" {
 			cacheDir, err := os.UserCacheDir()
 			if err != nil {
 				return "", fmt.Errorf("unable to find user cache directory: %s", err)
 			}
 
-			installPath = path.Join(cacheDir, "Programs", "binmate", "bin")
+			installPath = path.Join(cacheDir, "binmate", "bin")
 		} else {
+			homeDir, err := os.UserHomeDir()
+			if err != nil {
+				return "", fmt.Errorf("unable to find user home directory: %s", err)
+			}
+
 			installPath = path.Join(homeDir, ".local", "bin")
 		}
 	}
