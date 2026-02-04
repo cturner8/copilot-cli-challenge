71dc5fe5f90370d9d50cf51796f2fcef7e892b24
cameron turner <17177161+cturner8@users.noreply.github.com>
2026-02-04 20:26:47 +0000

    move db sync in sync command to pre run

--- internal/cli/root/command.go
+++ internal/cli/root/command.go
@@ -23,12 +23,14 @@ func NewCommand() *cobra.Command {
 		Long:          `An application for managing binary installations from remote repositories.`,
 		SilenceUsage:  true,
 		SilenceErrors: false,
-		RunE: func(cmd *cobra.Command, args []string) error {
+		PreRunE: func(cmd *cobra.Command, args []string) error {
 			// Sync config to database before launching TUI
 			if err := config.SyncToDatabase(*Config, DBService); err != nil {
 				return fmt.Errorf("sync error: %w", err)
 			}
-
+			return nil
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
 			p := tui.InitProgram(DBService, Config)
 			if _, err := p.Run(); err != nil {
 				return fmt.Errorf("TUI error: %w", err)
